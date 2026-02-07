d1c92c18f035470da259666bb33cce12ac5ea5b6
kobal09 <kobal0901@gmail.com>
2026-02-07 00:58:01 -0500

    empezando a crear la eplicacion

---
+++ src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -0,0 +1,24 @@
+package com.aluracursos.screenmatch.model;
+
+public enum Categoria {
+    ACCION("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIMEN("Crime");
+
+    private String categoriaOMDB;
+
+    Categoria (String categoriaOMDB){
+        this.categoriaOMDB = categoriaOMDB;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOMDB.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
+    }
+}
