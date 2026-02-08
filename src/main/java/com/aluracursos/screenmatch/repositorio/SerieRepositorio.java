34b7e429c0fee358ca3e4b49c96c70bfd3cf9112
kobal09 <kobal0901@gmail.com>
2026-02-07 19:27:07 -0500

    conexion a una base de datos y primeros pasos del crud

---
+++ src/main/java/com/aluracursos/screenmatch/repositorio/SerieRepositorio.java
@@ -0,0 +1,7 @@
+package com.aluracursos.screenmatch.repositorio;
+
+import com.aluracursos.screenmatch.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SerieRepositorio extends JpaRepository<Serie, Long> {
+}
